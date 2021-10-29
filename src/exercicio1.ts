8fc3ddf0b89d57f2f3b19f463bafe832d13f2cd2
Julia Dutra <juliadutragoncalves@gmail.com>
2021-10-28 23:05:01 -0300

    Refactor: movi a funcao do arquivo HTML para o TS; padrozinei o idioma dos nomes das funcoes e variaveis para ingles; adicionei comentarios ao codigo; alterei de let para const variaveis que nao eram reatribuidas.

--- src/exercicio1.ts
+++ src/exercicio1.ts
@@ -1,5 +1,6 @@
-let vowels : Array<string> = ["a", "e", "i", "o", "u"];
+const vowels : Array<string> = ["a", "e", "i", "o", "u"];
 
+/* Funcao que recebe como parametro uma palavra e retorna a quantidade de vogais contidas nela*/
 function countVowels(word: string) : number {
     let numberOfVowels : number = 0
     for(let character of word) {
@@ -9,5 +10,15 @@ function countVowels(word: string) : number {
     }
     return numberOfVowels
 }
-const palavra = "origins"
-console.log(`A palavra ${palavra} possui ${countVowels(palavra)} vogais.`);
+
+/*Funcao acionada quando o usuario clica no botao 'Confirmar' contido no formulario HTML. Obtem
+ o valor digitado e conta a quantidade de vogais*/
+
+function onButtonClick() {
+    const word = (<HTMLInputElement>document.getElementById("wordField")).value
+    const number = countVowels(word)
+    alert(`A palavra ${word} possui ${number} vogais.`)
+ }
+
+const sampleWord = "origins"
+console.log(`A palavra ${sampleWord} possui ${countVowels(sampleWord)} vogais.`);
